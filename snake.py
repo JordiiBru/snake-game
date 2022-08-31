a579c151560b436bf1ecc9030011177c1b2115bb
jordiibru <jordibrucarci@gmail.com>
2022-08-31 14:01:20 +0200

    img moving and CLASSES

--- snake.py
+++ snake.py
@@ -1,30 +1,58 @@
-import pygame, sys
-pygame.init()
+import pygame
+from pygame.locals import *
 
-size = width, height = 400, 450
-speed = [2,2]
-black = 0,0,0
 
-screen = pygame.display.set_mode(size)
-ball = pygame.image.load("poke.png")
-ballrect = ball.get_rect()
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-    
-    ballrect = ballrect.move(speed)
-    
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-    
+class Game:
+    def __init__ (self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000,750))
+        self.screen.fill((110,110,5))
+        self.snake = Snake(self.screen)
+        self.snake.draw()
 
-    screen.fill(black)
-    screen.blit(ball,ballrect)
-    pygame.display.flip()
+    def run(self):
+        self.crashed = False
+        while not self.crashed:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: crashed = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.snake.move_up()
+                    if event.key == pygame.K_DOWN: 
+                        self.snake.move_down()
+                    if event.key == pygame.K_LEFT: 
+                        self.snake.move_left()
+                    if event.key == pygame.K_RIGHT: 
+                        self.snake.move_right()
+                    if event.key == pygame.K_ESCAPE:
+                        self.crashed = True
 
-    """
-    next milestone: move pokeball with cursors
-    """
+class Snake:
+    def __init__ (self, dad_screen):
+        self.dad_screen = dad_screen
+        self.ball = pygame.image.load("poke.png")
+        self.x = 475
+        self.y = 350
+    
+    def draw(self):
+        self.dad_screen.fill((110,110,5))
+        self.dad_screen.blit(self.ball, (self.x,self.y))
+        pygame.display.flip()
+    
+    def move_up(self):
+        self.y -= 25
+        self.draw()
+    def move_down(self):
+        self.y += 25
+        self.draw()
+    def move_left(self):
+        self.x -= 25
+        self.draw()
+    def move_right(self):
+        self.x += 25
+        self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
