87d14ddcf2fbeaa75f2dd09694f67e968ea88331
jordiibru <jordibrucarci@gmail.com>
2022-08-31 15:37:56 +0200

    movement + apple

--- snake.py
+++ snake.py
@@ -1,21 +1,29 @@
 import pygame
 from pygame.locals import *
+import time
 
+SIZE = 50
 
 class Game:
     def __init__ (self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1000,750))
+        self.screen = pygame.display.set_mode((1000,800))
         self.screen.fill((110,110,5))
-        self.snake = Snake(self.screen)
+        self.snake = mySnake(self.screen,6)
         self.snake.draw()
+        self.apple = myApple(self.screen)
+        self.apple.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
 
     def run(self):
         self.crashed = False
         while not self.crashed:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT: crashed = True
-                if event.type == pygame.KEYDOWN:
+                if event.type == pygame.QUIT: self.crashed = True
+                elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
                         self.snake.move_up()
                     if event.key == pygame.K_DOWN: 
@@ -27,31 +35,55 @@ class Game:
                     if event.key == pygame.K_ESCAPE:
                         self.crashed = True
 
-class Snake:
-    def __init__ (self, dad_screen):
+            self.play()
+            time.sleep(0.2)
+
+class mySnake:
+    def __init__ (self, dad_screen, length):
         self.dad_screen = dad_screen
-        self.ball = pygame.image.load("poke.png")
-        self.x = 475
-        self.y = 350
+        self.length = length
+        self.player = pygame.image.load("corpse1.png")
+        self.x = [SIZE] * length
+        self.y = [SIZE] * length
+        self.direction = 'right'
     
     def draw(self):
-        self.dad_screen.fill((110,110,5))
-        self.dad_screen.blit(self.ball, (self.x,self.y))
+        self.dad_screen.fill((171, 94, 7))
+        for i in range(self.length):
+            self.dad_screen.blit(self.player, (self.x[i],self.y[i]))
         pygame.display.flip()
     
     def move_up(self):
-        self.y -= 25
-        self.draw()
+        self.direction = 'up'
     def move_down(self):
-        self.y += 25
-        self.draw()
+        self.direction = 'down'
     def move_left(self):
-        self.x -= 25
-        self.draw()
+        self.direction = 'left'
     def move_right(self):
-        self.x += 25
+        self.direction = 'right'
+    
+    def walk(self):
+        "only the head has the new direction and they pass its command to each other"
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+        
+        if self.direction == 'up': self.y[0] -= SIZE
+        if self.direction == 'down': self.y[0] += SIZE
+        if self.direction == 'left': self.x[0] -= SIZE
+        if self.direction == 'right': self.x[0] += SIZE
         self.draw()
 
+class myApple:
+    def __init__ (self, dad_screen):
+        self.dad_screen = dad_screen
+        self.fruit = pygame.image.load("apple.png")
+        self.x = SIZE*3
+        self.y = SIZE*3
+
+    def draw(self):
+        self.dad_screen.blit(self.fruit, ((self.x,self.y)))
+        pygame.display.flip()
 
 if __name__ == '__main__':
     game = Game()
